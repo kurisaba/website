ce9920cc357273002987acacf113bac4da7cf154
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-23 09:49:14 +0000

    fix Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,35 +6,35 @@ export default function Header() {
           class="flex bg-header w-full fixed z-[100] top-0 left-0 text-sm sm:text-2xl font-medium text-white items-center justify-center z-[999]"
           style={{ background: "rgba(0, 0, 0, 0.5)" }}
         >
-          <a class=" mr-2 rounded-lg p-4 mt-1 opacity-100" href="/">
+          <a class="rounded-lg p-4 mt-1 opacity-100 scale-[0.925]" href="/">
             Home
           </a>
           <a
-            class=" mr-2 rounded-lg p-3 mt-1 opacity-100"
+            class="rounded-lg p-3 mt-1 opacity-100 scale-[0.925]"
             href="https://wiki.kurisaba.com"
           >
             WIKI
           </a>
           <a
-            class=" mr-2 rounded-lg p-3 mt-1 opacity-100"
+            class="rounded-lg p-3 mt-1 opacity-100 scale-[0.925]"
             href="https://discord.gg/M9bsDXZrjK"
           >
             DISCORD
           </a>
           <a
-            class=" mr-2 rounded-lg p-3 mt-1 opacity-100"
+            class="rounded-lg p-3 mt-1 opacity-100 scale-[0.95]"
             href="https://x.com/Kurisaba_MC"
           >
             X
           </a>
           <a
-            class=" mr-2 rounded-lg p-3 mt-1 opacity-100"
+            class="rounded-lg p-3 mt-1 opacity-100 scale-[0.925]"
             href="https://www.youtube.com/@Kurisaba_MC"
           >
             YOUTUBE
           </a>
           <a
-            class=" mr-2 rounded-lg p-3 mt-1 opacity-100"
+            class="rounded-lg p-3 mt-1 opacity-100 scale-[0.925]"
             href="https://tex.kurisaba.com/pack.zip"
           >
             Texture
